67f81989b3b89ed3c59482055324e52b5e100fb1
sidab <info@sidab.ru>
2021-01-27 11:15:45 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -12,9 +12,9 @@ app = new Framework7({
     version: 1.0,
     init: false,
     routes: routes,
-    backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.10/murad/hs/tsd',
+    //backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.10/murad/hs/tsd',
     //backend:'http://176.120.211.250/proxy.php?url=http://192.168.215.10/murad/hs/tsd',
-    //backend:'http://192.168.215.10/murad/hs/tsd',
+    backend:'http://192.168.215.10/murad/hs/tsd',
     view: {
         animate: false,
         iosDynamicNavbar: false
@@ -56,7 +56,11 @@ $(document).on('deviceready', function () {
         crossDomain: true,
         beforeSend: function (xhr) {
 
-            xhr.setRequestHeader('Authorization', 'Basic ' + btoa('obmen_tsd:12345'));
+            if (app.params.backend === 'http://192.168.215.10/murad/hs/tsd') {
+
+                xhr.setRequestHeader('Authorization', 'Basic ' + btoa('obmen_tsd:12345') );
+
+            }
 
         },
         complete: function (xhr) {
