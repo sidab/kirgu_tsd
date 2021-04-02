71baf3d9d36d509011315be6a748f02ab6eff91c
adammagomedov6217 <73518817+adammagomedov6217@users.noreply.github.com>
2021-04-02 14:11:43 +0300

    fix

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -12,12 +12,12 @@ app = new Framework7({
     version: 1.0,
     init: false,
     routes: routes,
-    //backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.9/sad/hs/tsd',
-    backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.10/murad/hs/tsd',
+    backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.9/sad/hs/tsd',
+    //backend:'http://192.168.200.110/proxy.php?url=http://192.168.215.10/murad/hs/tsd',
     //backend:'http://192.168.215.10/murad/hs/tsd',
     view: {
         animate: false,
-        iosDynamicNavbar: false
+        iosDynamicNavbar: false,
     },
     dialog: {
         buttonOk: 'Ок',
